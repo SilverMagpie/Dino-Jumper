0af74dd639b904df189584fc2964583e3dc491a5
daneagle12 <60154434+danielku-create@users.noreply.github.com>
2020-11-23 16:16:45 -0700

    Program now counts lives of the player and deletes obstacles.

--- dino_scratch.py
+++ dino_scratch.py
@@ -16,10 +16,11 @@ LEFT_VIEWPORT_MARGIN = 250
 RIGHT_VIEWPORT_MARGIN = 1000 - 249
 BOTTOM_VIEWPORT_MARGIN = 50
 TOP_VIEWPORT_MARGIN = 100
+NUM_LIVES = 5
 
 # Movement speed of player, in pixels per frame
-PLAYER_MOVEMENT_SPEED = 8
-PLAYER_JUMP_SPEED = 20
+PLAYER_MOVEMENT_SPEED = 12
+PLAYER_JUMP_SPEED = 25
 
 
 class Dino_Game(arcade.Window):
@@ -30,9 +31,10 @@ class Dino_Game(arcade.Window):
 
         self.view_bottom = 0
         self.view_left = 0
+        self.count_collisions = 0
 
         self.wall_list = arcade.SpriteList() #TODO: Create a class for this
-        self.wall_list = arcade.SpriteList() #TODO: Create a class for this
+        self.obstacle_list = arcade.SpriteList() #TODO: Create a class for this
         #self.ground = Ground()
         self.player_sprite = Player()
         self.width = SCREEN_WIDTH
@@ -54,8 +56,8 @@ class Dino_Game(arcade.Window):
             x += random.randint(50, 250)
             #create_obstacle = random.randint()
             obstacle = Obstacle()
-            obstacle.set_position(x, 32 + 50)
-            self.wall_list.append(obstacle)
+            obstacle.set_position(x, 32 + random.randint(38, 150))
+            self.obstacle_list.append(obstacle)
 
         self.player_list = arcade.SpriteList() #TODO: Create a class for this
         self.player_list.append(self.player_sprite)
@@ -70,7 +72,7 @@ class Dino_Game(arcade.Window):
 
         # Draw our sprites
         self.wall_list.draw()
-        self.wall_list.draw()
+        self.obstacle_list.draw()
         self.player_list.draw()
 
     def on_key_press(self, key, modifiers):
@@ -93,10 +95,27 @@ class Dino_Game(arcade.Window):
     def on_update(self, delta_time):
         """ Movement and game logic """
 
-        wall_list_hits = arcade.check_for_collision_with_list(self.player_sprite, self.wall_list)
+        obstacle_hits = arcade.check_for_collision_with_list(self.player_sprite, self.obstacle_list)
+        # print(obstacle_hits)
+        # print(f"Length of list: {len(obstacle_hits)}")
 
-        # if len(wall_list_hits) > 0:
-        #     self._background_color = (arcade.csscolor.RED)
+
+        for obstacle in obstacle_hits:
+            # Remove the coin
+            obstacle.remove_from_sprite_lists()
+            # Play a sound
+            #arcade.play_sound(self.collect_coin_sound)
+
+        if len(obstacle_hits) > 0:
+            self.count_collisions += 1
+
+
+        if self.count_collisions >= NUM_LIVES:
+            self._background_color = (arcade.csscolor.RED)
+            #arcade.pause(5)
+            print("You LOSE!")
+            arcade.close_window()
+
 
         self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
@@ -183,6 +202,12 @@ class Ground(arcade.Sprite):
     def get_id(self):
         return self._id
 
+    def update(self):
+        super().update()
+
+        if self.center_x < 0:
+            self.remove_from_sprite_lists()
+
 
 
 class Obstacle(arcade.Sprite):
@@ -201,6 +226,12 @@ class Obstacle(arcade.Sprite):
 
     def get_id(self):
         return self._id
+
+    def update(self):
+        super().update()
+
+        if self.center_x < 0:
+            self.remove_from_sprite_lists()
 
 
 if __name__ == "__main__":
